fd476ce94c1017936603042cdd2bca108b61f428
Junkang-Macbook-Air <yuanjunkang@gmail.com>
2025-05-31 04:15:03 +0800

    toolz accumulate && test_accumulate

--- src/Toolz/Itertoolz.fs
+++ src/Toolz/Itertoolz.fs
@@ -3,23 +3,11 @@ open FSharpPlus
 
 module Itertoolz =
 
-    type Tail =
-        static member Tail (x: 'T list) = List.tail x
-        static member Tail (x: 'T array) = Array.tail x
-        static member Tail (x: 'T seq) = Seq.tail x
-
-        static member inline Invoke source =
-            let inline call_2 (a: ^a, b: ^b) = ((^a or ^b) : (static member Tail : _ -> _) b)
-            let inline call (a: 'a, b: 'b) = call_2 (a, b)
-            call (Unchecked.defaultof<Tail>, source)
-
     let inline accumulate (binop: 'a -> 'a -> 'a) seq (initial: 'a option) =
-        let inline tail source = Tail.Invoke source
-
         match initial with
         | Some init -> scan binop init seq
         | None ->
             match tryHead seq with
-            | Some head -> scan binop head (tail seq)
+            | Some head -> scan binop head (skip 1 seq)
             | None -> empty
         
---
+++ test/Toolz.Tests/ItertoolzTests.fs
@@ -0,0 +1,29 @@
+﻿module ItertoolzTests
+
+open System
+open Xunit
+open FsUnit.Xunit
+open FsCheck.Xunit
+open operator
+open Toolz.Itertoolz
+
+let identity x = x
+let iseven x = x % 2 = 0
+let isodd x = x % 2 = 1
+let inc x = x + 1
+let double x = 2 * x
+
+[<Fact>]
+let ``test_accumulate`` () =
+    accumulate add [1; 2; 3; 4; 5] None |> should equal [1; 3; 6; 10; 15]
+    accumulate mul [1; 2; 3; 4; 5] None |> should equal [1; 2; 6; 24; 120]
+    accumulate add [1; 2; 3; 4; 5] (Some -1) |> should equal [-1; 0; 2; 5; 9; 14]
+
+    let binop a b =
+        System.Diagnostics.Debug.Assert(false, "binop should not be called")
+        0
+
+    // start = object()
+    // assert list(accumulate(binop, [], start)) == [start]
+    accumulate binop [] None |> should equal ([] |> List.map ignore)
+    // assert list(accumulate(add, [1, 2, 3], no_default2)) == [1, 3, 6]
