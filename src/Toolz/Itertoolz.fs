b51675a636e86d6e018b415f9ba86e0069f1f0fc
Junkang-Macbook-Air <yuanjunkang@gmail.com>
2025-05-31 03:19:01 +0800

    toolz.accumulate

---
+++ src/Toolz/Itertoolz.fs
@@ -0,0 +1,23 @@
+﻿namespace Toolz
+open FSharpPlus
+
+module Itertoolz =
+
+    type GenericTail = Func with
+        static member Tail (x: 'T list) = List.tail x
+        static member Tail (x: 'T array) = Array.tail x
+        static member Tail (x: 'T seq) = Seq.tail x
+
+    let inline tail x = 
+        let inline tail (func: ^f) (x: ^C) : 'U = 
+            ((^f or ^C) : (static member Tail : ^C -> 'U) x)
+        tail Func x
+
+    let inline accumulate (binop: 'a -> 'a -> 'a) seq (initial: 'a option) =
+        match initial with
+        | Some init -> scan binop init seq
+        | None ->
+            match tryHead seq with
+            | Some head -> scan binop head (tail seq)
+            | None -> empty
+        
