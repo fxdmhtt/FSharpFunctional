237e7deaeb39b17b83af9ad70cd7f42e99e2c633
junkang <yuanjunkang@gmail.com>
2025-06-11 17:21:19 +0800

    string completed

--- src/Ramda/String.fs
+++ src/Ramda/String.fs
@@ -1,7 +1,28 @@
 ﻿namespace Ramda
+
 open FSharpPlus
 
 [<AutoOpen>]
 module String =
-    let hello name =
-        printfn "Hello %s" name
+
+    let inline ``match`` (rx: string) (str: string) : string list =
+        System.Text.RegularExpressions.Regex.Matches(str, rx)
+        |> Seq.cast<System.Text.RegularExpressions.Match>
+        |> Seq.map (fun m -> m.Value)
+        |> Seq.toList
+
+    let inline replace (pattern: string) (replacement: string) (str: string) : string =
+        System.Text.RegularExpressions.Regex.Replace(str, pattern, replacement)
+
+    let inline split (sep: string) (str: string) : string list = str.Split(sep) |> Seq.toList
+
+    let inline test (pattern: string) (str: string) : bool =
+        System.Text.RegularExpressions.Regex.IsMatch(str, pattern)
+
+    let inline toLower (str: string) : string = str.ToLowerInvariant()
+
+    let inline toString (``val``: obj) : string = ``val``.ToString()
+
+    let inline toUpper (str: string) : string = str.ToUpperInvariant()
+
+    let inline trim (str: string) : string = str.Trim()
--- test/Ramda.Tests/StringTests.fs
+++ test/Ramda.Tests/StringTests.fs
@@ -5,4 +5,52 @@ open Xunit
 open FSharpPlus
 open FsUnit.Xunit
 open FsCheck.Xunit
-open Ramda.String
+open Ramda
+
+[<Fact>]
+let ``test_match`` () =
+    ``match`` "([a-z]a)" "bananas" |> should equal [ "ba"; "na"; "na" ]
+    ``match`` "a" "b" |> should equal ([]: string list)
+    (fun () -> ``match`` "a" null |> ignore) |> should throw typeof<ArgumentNullException>
+
+[<Fact>]
+let ``test_replace`` () =
+    replace "foo" "bar" "foo foo foo" |> should equal "bar bar bar"
+
+[<Fact>]
+let ``test_split`` () =
+    let pathComponents = split "/"
+
+    tail (pathComponents "/usr/local/bin/node")
+    |> toList
+    |> should equal [ "usr"; "local"; "bin"; "node" ]
+
+    split "." "a.b.c.xyz.d" |> toList |> should equal [ "a"; "b"; "c"; "xyz"; "d" ]
+
+[<Fact>]
+let ``test_test`` () =
+    test "^x" "xyz" |> should equal true
+    test "^y" "xyz" |> should equal false
+
+[<Fact>]
+let ``test_toLower`` () = toLower "XYZ" |> should equal "xyz"
+
+[<Fact>]
+let ``test_toString`` () =
+    toString 42 |> should equal "42"
+    toString "\"abc\"" |> should equal "\"abc\""
+    toString [ 1; 2; 3 ] |> should equal "[1; 2; 3]"
+
+    toString {| foo = 1; bar = 2; baz = 3 |}
+    |> should equal "{ bar = 2\n  baz = 3\n  foo = 1 }"
+
+    toString (new DateTime(2001, 2, 3, 4, 5, 6, 0))
+    |> should equal "2001/2/3 4:05:06"
+
+[<Fact>]
+let ``test_toUpper`` () = toUpper "abc" |> should equal "ABC"
+
+[<Fact>]
+let ``test_trim`` () =
+    trim "   xyz  " |> should equal "xyz"
+    map trim (split "," "x, y, z") |> toList |> should equal [ "x"; "y"; "z" ]
